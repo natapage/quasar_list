86424361b871ce1dc852d69c649bc202a83c54e0
natapage <624832@gmail.com>
2023-08-31 11:41:18 +0300

    created list

---
+++ src/helper.js
@@ -0,0 +1,28 @@
+import { watch, ref, onMounted, onUnmounted } from "vue";
+
+const useLocalStorage = (key, defaultValue) => {
+  const value = ref(defaultValue);
+  const read = () => {
+    const v = window.localStorage.getItem(key);
+    if (v != null) value.value = JSON.parse(v);
+  };
+  read();
+
+  onMounted(() => {
+    window.addEventListener("storage", read);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("storage", read);
+  });
+
+  const write = () => {
+    window.localStorage.setItem(key, JSON.stringify(value.value));
+  };
+  watch([value], write);
+  return value;
+};
+
+export const useLocalNotes = () => {
+  return useLocalStorage("notes", []);
+};
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Container from "src/components/ContainerItem.vue";
+import NoteCard from "src/components/NoteCard.vue";
+import { useLocalNotes } from "src/helper";
+import { useRouter } from "vue-router";
+const notes = useLocalNotes();
+const router = useRouter();
+</script>
+
+<template>
+  <q-page padding>
+    <Container>
+      <div class="row items-center justify-between">
+        <h3>Your notes</h3>
+        <div>
+          <q-btn round color="positive" icon="add" to="/new"></q-btn>
+        </div>
+      </div>
+      <Note-card
+        v-for="({ title, description }, index) in notes"
+        :key="index"
+        :title="title"
+        :description="description"
+      />
+    </Container>
+  </q-page>
+</template>
